807ded230fe366f320eb54c795a21d8d3ab22474
殷保苗 <3077294628@qq.com>
2025-08-09 10:18:00 +0800

    chore: prepare public NearGo development baseline

--- src/main/java/com/hmdp/config/RedissonConfig.java
+++ src/main/java/com/hmdp/config/RedissonConfig.java
@@ -25,7 +25,11 @@ public class RedissonConfig {
     public RedissonClient redissonClient(){
         //配置
         Config config=new Config();
-        config.useSingleServer().setAddress("redis://"+host+":"+port).setPassword(password);
+        if (password == null || password.isEmpty()) {
+            config.useSingleServer().setAddress("redis://" + host + ":" + port);
+        } else {
+            config.useSingleServer().setAddress("redis://" + host + ":" + port).setPassword(password);
+        }
         //创建对并且返回
         return Redisson.create(config);
     }
